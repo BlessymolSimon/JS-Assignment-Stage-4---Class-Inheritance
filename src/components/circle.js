3363375bd60e64737ba5e391f08d54f9be909b7d
Blessymol Simon <blessy4mooc@gmail.com>
2020-10-31 00:12:31 +0530

    edited as per qn

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
+const Shape=require('./shape');
 
 // declare class
 
+class Circle extends Shape {
+    constructor()
+    {
+        super();
+    }
+    calculateArea() {
+        console.log("calculating area of circle");
+    }
+}
 
 // export class using module.exports
+module.exports=Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
-// declare class
+class Shape {
+    constructor() {
+        this.color="red";
+    }
+    drawShape(){
+        console.log("drawing a shape");
+    }
+    calculateArea() {
+        console.log("calculating area of a shape");
+    }
+}
 
-// export class using module.exports
+module.exports=Shape;
